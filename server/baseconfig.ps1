938ac2a3e9b236a7139066edaf8aef0bc495dac1
Kim-Andre Johansen <kim-andre.johansen@evry.com>
2016-11-12 21:02:01 +0100

    added IPv6 Config

---
+++ server/baseconfig.ps1
@@ -0,0 +1,15 @@
+Set-StrictMode -Off
+
+configuration BaseConfig {
+    node localhost {
+        Registry DisableIPv6 {
+            Force = "True"
+            Ensure = "Present"
+            Key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TCPIP6\Parameters"
+            ValueName = "DisabledComponents"
+            ValueData = "0x10111110"
+            ValueType = "Dword"
+            Hex = "True"
+        }
+    }
+}

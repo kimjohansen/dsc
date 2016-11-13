740d0d2dd540ab0be27d7c6b8b43f58d0eeb2180
Kim-Andre Johansen <kim-andre.johansen@evry.com>
2016-11-13 11:09:29 +0100

    testing

--- role/dhcp.ps1
+++ role/dhcp.ps1
@@ -15,6 +15,26 @@ configuration dhcp {
             Name = "DHCP"
             Ensure = "Present"
         }
+
+        xDhcpServerScope Scope{
+            DependsOn = '[WindowsFeature]DHCP'
+            Ensure = 'Present'
+            IPEndRange = '10.20.30.250'
+            IPStartRange = '10.20.30.5'
+            Name = 'PowerShellScope'
+            SubnetMask = '255.255.255.0'
+            LeaseDuration = '00:08:00'
+            State = 'Active'
+            AddressFamily = 'IPv4'
+        }
+
+        xDhcpServerOption Option{
+            Ensure = 'Present'
+            ScopeID = '10.20.30.0'
+            DnsDomain = 'fox.test'
+            DnsServerIPAddress = '10.20.30.1'
+            AddressFamily = 'IPv4'
+        }
     }
 }
 

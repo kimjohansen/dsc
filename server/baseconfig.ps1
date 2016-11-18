d91721629cbb442d3c48952c4883c55308693e6e
Kim-Andre Johansen <kim-andre.johansen@evry.com>
2016-11-18 21:14:35 +0100

    test

--- server/baseconfig.ps1
+++ server/baseconfig.ps1
@@ -2,6 +2,7 @@ configuration BaseConfig
 {
     node localhost
     {
+
         # Disable autostart of ServerManager on login
         Registry ServerManager
         {
@@ -9,6 +10,17 @@ configuration BaseConfig
             ValueName   = 'DoNotOpenServerManagerAtLogon'
             Force       =  $true
             ValueData   = '1'
+            ValueType   = 'Dword'
+        }
+
+        ## Network Configuration
+        #Disable IPv6
+        Registry IPv6
+        {
+            Key         = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TCPIP6\Parameters'
+            ValueName   = 'DisabledComponents'
+            Force       = $true
+            ValueData   = 'FF' 
             ValueType   = 'Dword'
         }
 

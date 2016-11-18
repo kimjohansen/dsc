cf99e29bd87e324d9b6180e0aac3f8cf70f7663f
Kim-Andre Johansen <kim-andre.johansen@evry.com>
2016-11-18 20:59:02 +0100

    test

--- server/baseconfig.ps1
+++ server/baseconfig.ps1
@@ -7,7 +7,7 @@ configuration BaseConfig
         {
             Key         = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager'
             ValueName   = 'DoNotOpenServerManagerAtLogon'
-            Force       =  True
+            Force       =  $true
             ValueData   = '1'
             ValueType   = 'Dword'
         }
